f8e87dbb46ebee8e5abbdb9356d786098cd77ac7
carloscuzik <cuzikjack@gmail.com>
2017-09-03 23:41:43 -0300

    consertando o bug da troca de linguagem

--- _assets/_js/script.js
+++ _assets/_js/script.js
@@ -1,3 +1,16 @@
+var codeEditor;
+function init () {
+	this.codeEditor = CodeMirror.fromTextArea(document.getElementById("code"), {
+		lineNumbers: true,
+		matchBrackets: true,
+		// styleActiveLine: true,
+    	// theme:"eclipse",
+		mode: lang_names["cpp"]
+	});
+	var mac = CodeMirror.keyMap.default == CodeMirror.keyMap.macDefault;
+  	CodeMirror.keyMap.default[(mac ? "Cmd" : "Ctrl") + "-Space"] = "autocomplete";
+}
+
 function identify_language () {
 	var element = document.getElementById("arq_name");
 	var language = '';
@@ -9,8 +22,15 @@ function identify_language () {
 	}
 }
 
+lang_names = {}
+lang_names["scala"] = "text/x-scala"
+lang_names["c"] = "text/x-csrc"
+lang_names["cpp"] = "text/x-c++src"
+lang_names["java"] = "text/x-java"
+lang_names["css"] = "text/css"
+lang_names["py"] = "text/x-python"
+lang_names["rb"] = "text/x-ruby"
+
 function change_language (language_new) {
-	// language_old = document.getElementById("code-block").classList[2];
-	// document.getElementById("code-block").classList.remove(language_old);
-	// document.getElementById("code-block").classList.add(language_new);
+	this.codeEditor.setOption("mode", lang_names[language_new]);
 }
